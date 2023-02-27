a2ead720fbda264f4dad2487840420a9710b7f0f
suryavarma97 <126458418+suryavarma97@users.noreply.github.com>
2023-02-27 16:06:00 +0530

    Update streamlit_app.py
    updated avocado

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 Streamlit.header('Breakfast Menu')
-Streamlit.text('Omega 3 & Blueberry Oatmeal')
-Streamlit.text('Kale, Spinach & Rocket Smoothie')
-Streamlit.text('Hard-Boiled Free-Range Egg')
+Streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+Streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+Streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
